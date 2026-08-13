b154c6e7880718162a5b3d2b78aa2c129d936e3b
ceduardopc <seduardocallau@gmail.com>
2026-08-13 10:31:47 -0400

    fix: add OneSignal service worker fallbacks to resolve 404/html mime type errors

---
+++ frontend/public/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,5 @@
+/**
+ * OneSignal SDK Service Worker (Updater)
+ * Actualizado desde el SDK web v16 de OneSignal.
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
---
+++ frontend/public/OneSignalSDKWorker.js
@@ -0,0 +1,5 @@
+/**
+ * OneSignal SDK Service Worker
+ * Actualizado desde el SDK web v16 de OneSignal.
+ */
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
